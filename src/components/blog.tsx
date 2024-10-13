8707846939630bb9766501ecb1a45f252481b171
CaffeineDuck <hello@samrid.me>
2024-10-13 14:22:22 +0545

    fix: use class attr instead of className for solid-js

--- src/components/blog.tsx
+++ src/components/blog.tsx
@@ -13,32 +13,32 @@ export const Blog = (props: BlogProps) => {
   const dateString = props.date.toISOString().split('T')[0];
 
   return (
-    <article className="w-full">
-      <a href={props.link} className="block h-full">
-        <div className="flex flex-col h-full justify-between transition-all hover:shadow-md rounded-lg border-slate-200 border p-4">
+    <article class="w-full">
+      <a href={props.link} class="block h-full">
+        <div class="flex flex-col h-full justify-between transition-all hover:shadow-md rounded-lg border-slate-200 border p-4">
           <div>
-            <div className="flex items-center gap-x-4 text-xs">
-              <time dateTime={dateString} className="text-gray-500">
+            <div class="flex items-center gap-x-4 text-xs">
+              <time dateTime={dateString} class="text-gray-500">
                 {dateString}
               </time>
-              <span className="relative z-10 rounded-full bg-gray-50 px-3 py-1.5 font-medium text-gray-600 hover:bg-gray-100">
+              <span class="relative z-10 rounded-full bg-gray-50 px-3 py-1.5 font-medium text-gray-600 hover:bg-gray-100">
                 Python
               </span>
             </div>
-            <h3 className="mt-3 text-lg font-semibold leading-6 text-gray-600 group-hover:text-gray-900">
+            <h3 class="mt-3 text-lg font-semibold leading-6 text-gray-600 group-hover:text-gray-900">
               {props.title}
             </h3>
-            <p className="mt-3 line-clamp-3 text-sm leading-6 text-gray-600">
+            <p class="mt-3 line-clamp-3 text-sm leading-6 text-gray-600">
               {props.description}
             </p>
           </div>
-          <div className="mt-4">
-            <ul className="text-sm leading-4 flex flex-wrap text-slate-800 mb-2">
-              {props.tags.slice(0, 3).map((tag, index) => (
-                <li key={index} className="mr-2 mb-2 border p-1 rounded-lg">{tag}</li>
+          <div class="mt-4">
+            <ul class="text-sm leading-4 flex flex-wrap text-slate-800 mb-2">
+              {props.tags.slice(0, 3).map(tag => (
+                <li class="mr-2 mb-2 border p-1 rounded-lg">{tag}</li>
               ))}
             </ul>
-            <div className="flex justify-end mt-2">
+            <div class="flex justify-end mt-2">
               <ArrowLink href={props.link} label="Read" />
             </div>
           </div>
--- src/components/my-blogs.tsx
+++ src/components/my-blogs.tsx
@@ -1,6 +1,6 @@
 import { Blog } from "./blog";
 import { Button } from "./button";
-import { BlogProps } from './blog.tsx'
+import { BlogProps } from './blog'
 
 interface MyBlogsProps {
   blogs: BlogProps[];
@@ -8,17 +8,17 @@ interface MyBlogsProps {
 
 export const MyBlogs = (props: MyBlogsProps) => {
   return (
-    <div className="relative py-10">
-      <section className="mx-3 relative" id="blogs">
-        <h1 className="tracking-[0.5px] text-3xl md:text-5xl text-center mb-10 text-black font-bold">
-          My <span className="text-red-600">Blogs</span>
+    <div class="relative py-10">
+      <section class="mx-3 relative" id="blogs">
+        <h1 class="tracking-[0.5px] text-3xl md:text-5xl text-center mb-10 text-black font-bold">
+          My <span class="text-red-600">Blogs</span>
         </h1>
-        <div className="px-4 md:px-10 mx-auto grid max-w-2xl grid-cols-1 gap-x-8 gap-y-16 sm:mt-16 lg:mx-0 lg:max-w-none lg:grid-cols-3">
-          {props.blogs.slice(0, 6).map((blogProp, index) => (
-            <Blog key={index} {...blogProp} />
+        <div class="px-4 md:px-10 mx-auto grid max-w-2xl grid-cols-1 gap-x-8 gap-y-16 sm:mt-16 lg:mx-0 lg:max-w-none lg:grid-cols-3">
+          {props.blogs.slice(0, 6).map(blogProp => (
+            <Blog {...blogProp} />
           ))}
         </div>
-        <div className="mt-16 flex justify-center md:justify-end px-4 md:px-10">
+        <div class="mt-16 flex justify-center md:justify-end px-4 md:px-10">
           <Button link="https://blog.samrid.me" cta="Read More" />
         </div>
       </section>
